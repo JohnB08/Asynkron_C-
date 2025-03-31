2304fcb2e959f7f70156c5771d44aa07b7e7afa2
johnb08 <johnb08.kodehode@gmail.com>
2025-03-31 12:13:16 +0200

    ekstra bit om threads

--- Program.cs
+++ Program.cs
@@ -19,6 +19,11 @@ static void WriteNewThread()
     for (int i  = 1000; i > 0; i--) Console.WriteLine(i);
 }
 
+//!!Thread har en del resursoverhead vs noen av de andre metodene for anynkron programmering som vi skal kikke på nedenfor.
+//!!Når vi bruker Thread nøkkelordet, er programmet vårt nødt å designere en ny "stack" og heap fra grunn av. 
+//!!Bruker vi andre metoder, som Task og async/await, tar vi nytte av en innebygget ThreadPool, et sett med ferdiginitierte execution threads
+//!!Som er ferdig spunnet opp, og tilgjengeliggjort for oss. 
+
 //Vi kan se for oss at vi får vårt program til å lage et eget subprogram som kjører parallellt på maskinen vår.
 
 //pga måten Console.WriteLine fungerer på, vil det komme ut i "chuncks" av resultat.
